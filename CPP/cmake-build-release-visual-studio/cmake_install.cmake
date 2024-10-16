a88861e9bdc8ce8b8e43c416ccc5c4ab9669976b
dboekestijn <david.boekestijn@outlook.com>
2024-10-16 15:06:05 +0200

    refactor / rename complete

--- CPP/cmake-build-release-visual-studio/cmake_install.cmake
+++ CPP/cmake-build-release-visual-studio/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/david/Projects/GitHub/Projects/Kaggle/kaggle-santa2023/CPP
+# Install script for directory: C:/Users/david/Projects/GitHub/Projects/Kaggle/kaggle-santa-2023/CPP
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/david/Projects/GitHub/Projects/Kaggle/kaggle-santa2023/CPP/cmake-build-release-visual-studio/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/david/Projects/GitHub/Projects/Kaggle/kaggle-santa-2023/CPP/cmake-build-release-visual-studio/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
